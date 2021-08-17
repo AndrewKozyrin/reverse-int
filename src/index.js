6f3d5a73cac1cd51fac0215c5de45de7c90864b7
Andrew <kozyrin.ai@gmail.com>
2021-08-17 12:25:11 +0400

    fix: delete console.log

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ module.exports = function reverse(n) {
     for (let i = str.length-1; i>=0; i--){
         reverseNumber = `${reverseNumber}${str[i]}`;
     }
-    console.log(n , +reverseNumber);
     return +reverseNumber;
 }
